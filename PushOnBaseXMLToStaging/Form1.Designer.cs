065e0e1c69521651f3bed3782a4b675a07efd695
JHardin1112 <jeremyhardin@gmail.com>
2018-08-15 15:04:04 -0400

    Add project files.

---
+++ PushOnBaseXMLToStaging/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PushOnBaseXMLToStaging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCopyFiles = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCopyFiles
+            // 
+            this.btnCopyFiles.Location = new System.Drawing.Point(12, 327);
+            this.btnCopyFiles.Name = "btnCopyFiles";
+            this.btnCopyFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnCopyFiles.TabIndex = 2;
+            this.btnCopyFiles.Text = "Copy Files";
+            this.btnCopyFiles.UseVisualStyleBackColor = true;
+            this.btnCopyFiles.Click += new System.EventHandler(this.btnCopyFiles_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 119);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(277, 202);
+            this.textBox1.TabIndex = 3;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(12, 31);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(277, 20);
+            this.txtSource.TabIndex = 4;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(12, 82);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(277, 20);
+            this.txtDestination.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Source Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Destination Directory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 362);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCopyFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Push XML to OnBase Staging";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCopyFiles;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PushOnBaseXMLToStaging/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PushOnBaseXMLToStaging
+{
+    public partial class Form1 : Form
+    {
+        private const string sourceLocation = @"\\192.168.2.12\e$\Archive_OnBaseXMLDestination";
+        private const string copyDestination = @"\\192.168.2.12\e$\OnBaseStageXML_RepushToGP";
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtSource.Text = sourceLocation;
+            txtDestination.Text = copyDestination;
+        }
+
+        private void btnCopyFiles_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>(
+                           textBox1.Text.Split(new string[] { "\r\n" },
+                           StringSplitOptions.RemoveEmptyEntries));
+
+            string singleLine = "";
+            string message = "";
+
+            for (int x = 0; x <= list.Count - 1; x++)
+            {
+                message += list[x] + ".xml" + Environment.NewLine;
+
+                if (x != list.Count - 1)
+                {
+                    singleLine += list[x].ToString() + ".xml " + " OR ";
+                }
+                else
+                {
+                    singleLine += list[x].ToString() + ".xml";
+                }
+            }
+
+            if (MessageBox.Show(@"Do you want to copy the following files from " + Environment.NewLine + Environment.NewLine
+                          + sourceLocation + Environment.NewLine + Environment.NewLine + " to " + Environment.NewLine + Environment.NewLine
+                          + copyDestination + ":" + Environment.NewLine + Environment.NewLine + message, "Confirm file copy", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+                string missingFiles = "";
+                int missingFilesCount = 0;
+                int filesCopiedCount = 0;
+
+                for (int x = 0; x <= list.Count - 1; x++)
+                {
+                    if (File.Exists(sourceLocation + @"\" + list[x] + ".xml"))
+                    {
+                        File.Copy(sourceLocation + @"\" + list[x] + ".xml", copyDestination + @"\" + list[x] + ".xml", true);
+                        filesCopiedCount += 1;
+                    }
+                    else
+                    {
+                        missingFiles += sourceLocation + @"\" + list[x] + ".xml" + Environment.NewLine;
+                        missingFilesCount += 1;
+                    }
+                }
+
+                if (filesCopiedCount > 0)
+                {
+                    MessageBox.Show("Files copied over succesfully.", "Job Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                if (missingFilesCount > 0)
+                {
+                    MessageBox.Show("The following files were not found: " + Environment.NewLine + Environment.NewLine + missingFiles + Environment.NewLine + Environment.NewLine + "This list has been copied to your Clipboard for your convenience.", "Missing Files", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Clipboard.SetText(missingFiles);
+                }
+
+            }
+        }
+
+    }
+}
